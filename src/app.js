26bde69f2ee444f6d692d395bd0dafd97c74682a
Erin Gurley <erinmgurley@gmail.com>
2019-04-25 15:37:55 -0700

    called feedCaterpillar function and removed placeholder body parts

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
 import feedCaterpillar from './feed-caterpillar.js';
 const fruitButtons = document.querySelectorAll('.fruit');
+const caterpillar = document.getElementById('caterpillar');
 
 for(let i = 0; i < fruitButtons.length; i++) {
     const fruitButton = fruitButtons[i];
 
     fruitButton.addEventListener('click', () => {
-        
+        feedCaterpillar(fruitButton.value, caterpillar);    
     });
 }
